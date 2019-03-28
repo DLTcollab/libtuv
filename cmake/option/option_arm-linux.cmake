e4d7037189dd93dcae29c67f5c88966ead0946ee
marktwtn <marktwtn@gmail.com>
2019-03-28 11:41:09 +0800

    Support Raspberry Pi 3 board with OS Linux

--- cmake/option/option_arm-linux.cmake
+++ cmake/option/option_arm-linux.cmake
@@ -26,6 +26,13 @@ if(DEFINED TARGET_BOARD)
           "-mfloat-abi=hard"
           "-mfpu=neon-vfpv4"
           )
+  elseif(${TARGET_BOARD} STREQUAL "rpi3")
+    # rpi3 specific
+    set(FLAGS_COMMON
+          ${FLAGS_COMMON}
+          "-mfloat-abi=softfp"
+          "-mfpu=neon-vfpv4"
+          )
   else()
     message(FATAL_ERROR "TARGET_BOARD=`${TARGET_BOARD}` is unknown to make")
   endif()
